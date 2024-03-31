61f7166fad6b6f5709a7a1bef6f84a8bde69938a
Kakhaber Mchedlishvili <kakha81@gmail.com>
2024-03-31 17:01:11 +0400

    add counter functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
-  return <div className='App'>hello world</div>;
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  console.log(counter);
+  return (
+    <div className='App'>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(10))}>-</button>
+      {isLogged ? <h3>Valuable Information I shouldn't sea</h3> : ''}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import allReducers from './reducers/index';
 import { Provider } from 'react-redux';
+// import { configureStore } from '@reduxjs/toolkit';
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
